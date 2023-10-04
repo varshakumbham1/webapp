46bc99c5724f347f27493699e8406f64efa0c8c5
Varsha Reddy Kumbham <kumbham.v@northeastern.edu>
2023-10-04 19:52:08 -0400

    Changed integration test

--- Tests/healthz.test.js
+++ Tests/healthz.test.js
@@ -1,24 +1,18 @@
-const authenticate = require('../src/routes/Healthz')
-const http = require('http');
-const server = http.createServer(async (req, res) => {
-    await authenticate(req, res);
-  });
+const supertest = require('supertest');
+const app = require('../app');
+var assert = require('assert');
 
-const testReq = http.request({
-    method: 'GET',
-    host: 'localhost',
-    path: '/healthz',
-    port: 3000, 
-  }, (res) => {
-    if (res.statusCode === 200) {
-      console.log('Test Passed: Successful authentication returns 200');
-    } else {
-      console.error('Test Failed: Successful authentication should return 200');
-    }
-  });
-  
-  testReq.end();
-
-  server.close(() => {
-    console.log('All tests completed. Server closed.');
-  });
+describe('Testing our Application', function () {
+    it('Simple assert test', function () {
+        assert.equal(1, 1);
+    });
+    it('GET /healthz end point of the application', (done) => {
+        supertest(app)
+            .get('/healthz')
+            .expect(200)
+            .end((err, response) => {
+                if (err) return done(err)
+                return done()
+        })
+    })
+})
--- app.js
+++ app.js
@@ -5,37 +5,15 @@ const User = require('./src/models/User')
 const Assignment = require('./src/models/Assignment')
 const { authenticate, getCredentials } = require('./auth')
 const assignmentRouter = require('./src/routes/Assignment');
-const healthzRouter = require('./src/routes/Healthz')
+//const healthzRouter = require('./src/routes/Healthz')
 const app = express()
 const port = 3000;
 const insert_row = require('./src/database/read_csv')
 app.use(express.json());
 
-const createDatabase = async () => {
-    //const { host, username, password, database } = config;
-    const database = 'Cloud_db'
-    const connection = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: 'Geethareddy@1989'
-    });
-     connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
-  };
-
-app.listen(port, async () => {
-    console.log(`Server is running on port ${port}`);
-  });
-
-(async () => {
-try {
-    await createDatabase();
-    await sequelize.sync({ alter: true }).then(() => {
-        insert_row()
-    })
-} catch (error) {
-    console.error("Error:", error);
-}
-})();
+sequelize.sync({ alter: true }).then(() => {
+    insert_row()
+})
 
 User.hasMany(Assignment, {
     foreignKey: 'user_id', 
@@ -44,29 +22,26 @@ Assignment.belongsTo(User, {
     foreignKey: 'user_id', 
 });
 
-
-
 app.use('/assignments', assignmentRouter);
-app.use('/healthz', healthzRouter);
 
-// app.get('/healthz', async (req, res) => {
-//     try {
-//         res.set('Cache-Control', 'no-cache');
-//         if(Object.keys(req.body).length > 0) {
-//             res.status(400).send();
-//         }
-//         if(Object.keys(req.query).length > 0) {
-//             res.status(400).send()
-//         }
-//         else {
-//             await sequelize.authenticate()
-//             res.status(200).send()
-//         }
-//       } catch (error) {
-//             res.status(503).send()
-//       }
-// });
+app.get('/healthz', async (req, res) => {
+    try {
+        res.set('Cache-Control', 'no-cache');
+        if(Object.keys(req.body).length > 0) {
+            res.status(400).send();
+        }
+        if(Object.keys(req.query).length > 0) {
+            res.status(400).send()
+        }
+        else {
+            await sequelize.authenticate()
+            res.status(200).send()
+        }
+      } catch (error) {
+            res.status(503).send()
+      }
+});
 
-// app.use('/healthz', healthzRouter, (req, res, next) => {
-    
-// });
+app.listen(port, async () => {
+    console.log(`Server is running on port ${port}`);
+});
